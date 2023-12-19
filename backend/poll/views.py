3bd456f9dffab20c4fe2bde893f39b922c420e44
Jesus Jimenez <jesusjmz67@gmail.com>
2023-12-19 12:56:59 -0500

    drafted views and URLS, wrote and tested the serializers, created the models and made the migrations, the models are accesible on the django admin panel

--- backend/poll/views.py
+++ backend/poll/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Poll, Choice
+from .serializers import PollSerializer
 
-# Create your views here.
+class PollDetailView(APIView):
+    """
+    A view that returns a specific poll by its id.
+    """
+
+    def get(self, request, pk, format=None):
+        try:
+            poll = Poll.objects.get(pk=pk)
+        except Poll.DoesNotExist:
+            return Response({'error': 'Poll not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PollSerializer(poll)
+        return Response(serializer.data)
